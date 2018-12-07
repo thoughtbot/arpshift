7d6a3fcab6a69048ce8e50a6258ff11aa36eabe1
Edward Loveall <edward@thoughtbot.com>
2018-12-07 09:48:20 -0500

    duck audio based on how many notes are playing

--- src/static/audio.js
+++ src/static/audio.js
@@ -1,5 +1,10 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
+const mixer = audioCtx.createGain();
+const baseGain = 0.9;
+mixer.gain.setValueAtTime(baseGain, audioCtx.currentTime);
+mixer.connect(audioCtx.destination);
+let noteCount = 0;
 
 export default class Audio {
   constructor() {
@@ -11,17 +16,34 @@ export default class Audio {
     const freq = Math.pow(2, (noteNumber - 69) / 12) * 440;
     const env = this._generateEnvelope();
     const osc = audioCtx.createOscillator();
-    osc.type = "sine";
+    osc.type = "triangle";
     osc.frequency.value = freq;
 
-    env.connect(audioCtx.destination);
+    env.connect(mixer);
     osc.connect(env);
     osc.start();
+    this.addNote();
     osc.stop(audioCtx.currentTime + this.envTime);
+    setTimeout(this.removeNote, this.envTime * 1000);
   }
 
   get envTime() {
     return this.attackTime + this.releaseTime;
+  }
+
+  addNote() {
+    noteCount += 1;
+    this.rebalanceMixer();
+  }
+
+  removeNote() {
+    noteCount -= 1;
+    this.rebalanceMixer();
+  }
+
+  rebalanceMixer() {
+    const volume = baseGain / (noteCount * 0.8);
+    mixer.gain.setValueAtTime(volume, audioCtx.currentTime);
   }
 
   _generateEnvelope() {
